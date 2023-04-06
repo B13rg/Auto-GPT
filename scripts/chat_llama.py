1aacc1b2002d214c5ad98ad8b09764796c2f25fc
Iceberg <dev@icebergtech.xyz>
2023-04-06 11:02:15 -0700

    testing

--- scripts/chat_llama.py
+++ scripts/chat_llama.py
@@ -1,11 +1,12 @@
 import time
+import traceback
 import token_counter
 from config import Config
 from dotenv import load_dotenv
 from llama_cpp import Llama
 
 cfg = Config()
-llm = Llama(model_path='/models/7b/model.bin')
+llm = Llama(model_path='/models/7b/model.bin',n_batch=6000)
 
 def create_chat_message(role, content):
     """
@@ -37,7 +38,7 @@ def tokenize_chat_ctx(messages):
         for message in messages
     )
     prompt = f" \n\n### Instructions:{instructions}\n\n### Inputs:{chat_history}\n\n### Response:\nassistant: "
-    return llm.tokenize(prompt.encode('utf-8'))
+    return len(llm.tokenize(prompt.encode('utf-8')))
 
 def chat_with_llama(
         prompt,
@@ -67,46 +68,49 @@ def chat_with_llama(
                     "system", f"Permanent memory: {permanent_memory}")]
             
             user_input = [create_chat_message("user", user_input)]
-            
-            if debug:
-                print(f"Token limit: {token_limit}")
+
+            #if debug:
+            print(f"Token limit: {token_limit}")
+            print(str(tokenize_chat_ctx([create_chat_message("user", "test test test")])))
             # Count the currently used tokens plus the user input tokens for counting
             # Reserve 1000 tokens for the response
-            send_token_limit = token_limit - 1000 #- tokenize_chat_ctx(current_context) - tokenize_chat_ctx(user_input)
+
+            send_token_limit = 2048 - 100 - tokenize_chat_ctx(current_context) - tokenize_chat_ctx(user_input)-1040
 
             # Start appending messages to current context until we near the limit
             new_messages = []
             new_message_tokens = 0
             #Go through the most recent messages
-            for message in list.reverse(full_message_history):
+            for message in list(reversed(full_message_history)):
                 new_message_tokens += len(llm.tokenize([message]))
                 if new_message_tokens > send_token_limit:
                     break
                 
                 new_messages.insert(0, message)
 
+            print("Chose tokens")
             # Add the most recent message to the start of the current context, after the two system prompts created above
-            current_context.append(new_messages)
+            current_context = current_context + new_messages + user_input
             # Append user input, the length of this is accounted for above
-            current_context.append(user_input)
+            #current_context.append(user_input)
 
             # Calculate remaining tokens
-            tokens_remaining = token_limit - token_counter(current_context)
+            tokens_remaining = token_limit - tokenize_chat_ctx(current_context)
 
             # Debug print the current context
-            if debug:
-                print(f"Token limit: {token_limit}")
-                print(f"Send Token Count: {current_tokens_used}")
-                print(f"Tokens remaining for response: {tokens_remaining}")
-                print("------------ CONTEXT SENT TO AI ---------------")
-                for message in current_context:
-                    # Skip printing the prompt
-                    if message["role"] == "system" and message["content"] == prompt:
-                        continue
-                    print(
-                        f"{message['role'].capitalize()}: {message['content']}")
-                    print()
-                print("----------- END OF CONTEXT ----------------")
+            #if debug:
+            print(f"Token limit: {token_limit}")
+            print(f"Send Token Count: {token_limit-tokens_remaining}")
+            print(f"Tokens remaining for response: {tokens_remaining}")
+            print("------------ CONTEXT SENT TO AI ---------------")
+            for message in current_context:
+                # Skip printing the prompt
+                if message["role"] == "system" and message["content"] == prompt:
+                    continue
+                print(
+                    f"{message['role'].capitalize()}: {message['content']}")
+                print()
+            print("----------- END OF CONTEXT ----------------")
 
             assistant_reply = create_chat_completion(
                 messages=current_context,
@@ -120,9 +124,11 @@ def chat_with_llama(
                     "assistant", assistant_reply))
 
             return assistant_reply
-        except:
+        except Exception as ex:
             # TODO: WHen we switch to langchain, this is built in
             print("Error: ", "Shits broke")
+            print(ex)
+            print(traceback.format_exc())
             time.sleep(1)
 
 # Overly simple abstraction until we create something better
